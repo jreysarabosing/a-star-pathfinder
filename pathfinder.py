0ef9f11af56e6e89694091975e48e6c72daf31c0
Sarabosing, J. A <j.a.sarabosing@accenture.com>
2020-10-22 00:15:25 +0800

    finished check-neighbors and algo with path visual

--- pathfinder.py
+++ pathfinder.py
@@ -12,7 +12,6 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
-PURPLE = (128, 0, 128)
 ORANGE = (255, 165, 0)
 TURQUOISE = (64, 224, 208)
 BLACK = (0, 0, 0)
@@ -73,18 +72,85 @@ class Node:
 		pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
 
 	def update_neighbors(self, grid):
-		# PLACEHOLDER
-		pass
+		self.neighbors = []
+		# Check if node above the current node is a valid neighbor
+		if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():
+			self.neighbors.append(grid[self.row - 1][self.col])
+
+		# Check if node below the current node is a valid neighbor
+		if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier():
+			self.neighbors.append(grid[self.row + 1][self.col])
+
+		# Check if node to the left of the current node is a valid neighbor
+		if self.row > 0 and not grid[self.row][self.col - 1].is_barrier():
+			self.neighbors.append(grid[self.row][self.col - 1])
+
+		# Check if node to the right of the current node is a valid neighbor
+		if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():
+			self.neighbors.append(grid[self.row][self.col + 1])
 
 	def __lt__(self, other):
 		return False
 
 def h(point1, point2):
-	x1, x2 = point1
-	y1, y2 = point2
+	x1, y1 = point1
+	x2, y2 = point2
 
 	# Manhatten distance formula
 	return abs(x1 - x2) + abs(y1 - y2)
+
+def make_path(came_from, current_node, draw):
+	while current_node in came_from:
+		current_node = came_from[current_node]
+		current_node.make_path()
+		draw()
+
+def algorithm(draw, grid, start, end):
+	count = 0
+	open_set = PriorityQueue()
+	open_set.put((0, count, start))
+	came_from = {}
+	g_score = {node: float('inf') for row in grid for node in row}
+	g_score[start] = 0
+	f_score = {node: float('inf') for row in grid for node in row}
+	f_score[start] = h(start.get_position(), end.get_position())
+	
+	open_set_hash = {start}
+
+	# G_SCORE is the distance from start node to current node
+	# H_SCORE (from h(point1, point2)) is the estimated distance to the end node
+	# F_SCORE is the sum of G_SCORE & H_SCORE
+	# The lowest F_SCORE path is the shortest path from start node to end node
+
+	while not open_set.empty():
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		current_node = open_set.get()[2]
+		open_set_hash.remove(current_node)
+
+		if current_node == end:
+			make_path(came_from, end, draw)
+			start.make_start()
+			end.make_end()
+			return True
+
+		for neighbor in current_node.neighbors:
+			temp_g_score = g_score[current_node] + 1
+			if temp_g_score < g_score[neighbor]:
+				came_from[neighbor] = current_node
+				g_score[neighbor] = temp_g_score
+				f_score[neighbor] = temp_g_score + h(neighbor.get_position(), end.get_position())
+				if neighbor not in open_set_hash:
+					count += 1
+					open_set.put((f_score[neighbor], count, neighbor))
+					open_set_hash.add(neighbor)
+					neighbor.make_open()
+		draw()
+		if current_node != start:
+			current_node.make_closed()
+
+	return False
 
 def make_grid(rows, width):
 	grid = []
@@ -113,7 +179,7 @@ def draw(win, grid, rows, width):
 	draw_grid(win, rows, width)
 	pygame.display.update()
 
-def get_clicked_pos(pos, rows, width):
+def get_clicked_position(pos, rows, width):
 	gap = width // rows
 	y, x = pos
 	row = y // gap
@@ -126,18 +192,15 @@ def main(win, width):
 	start = None
 	end = None
 	run = True
-	started = False
 
 	while(run):
 		draw(win, grid, ROWS, width)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				run = False
-			if started:
-				continue
 			if pygame.mouse.get_pressed()[0]:
 				pos = pygame.mouse.get_pos()
-				row, col = get_clicked_pos(pos, ROWS, width)
+				row, col = get_clicked_position(pos, ROWS, width)
 				node = grid[row][col]
 				if not start:
 					start = node
@@ -149,13 +212,23 @@ def main(win, width):
 					node.make_barrier()
 			elif pygame.mouse.get_pressed()[2]:
 				pos = pygame.mouse.get_pos()
-				row, col = get_clicked_pos(pos, ROWS, width)
+				row, col = get_clicked_position(pos, ROWS, width)
 				node = grid[row][col]
 				node.reset()
 				if node == start:
 					start = None
 				elif node == end:
 					end = None
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_SPACE and start and end:
+					for row in grid:
+						for node in row:
+							node.update_neighbors(grid)
+					algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
+				if event.key == pygame.K_c:
+					start = None
+					end = None
+					grid = make_grid(ROWS, width)
 	pygame.quit()
 
 main(WIN, WIDTH)
